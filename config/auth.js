3ee06f21e4038da2afac22a58ae88896220275c3
Doctor01 <rchaud@bu.edu>
2019-04-25 11:53:17 -0400

    fixed callback link, working on view for search page

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '755083706406-onn87p71bdkmbrjs5612cd8jhtrek6e5.apps.googleusercontent.com',
         'clientSecret'  : 'EykjiG-5xB1ZxFnGYYLm17Fc',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
     }
 
 };
